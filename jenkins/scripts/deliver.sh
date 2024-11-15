b1adb2fbee7b3451b4ff1575d91ba94c0ff3764b
vinverma <47500906+vinverma@users.noreply.github.com>
2024-11-15 00:51:41 +0000

    Update deliver.sh
    removed pre and post set

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -10,17 +10,17 @@ mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-#set -x
+
 NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
-#set +x
+
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
-#set -x
+
 VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
-#set +x
+
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-#set -x
+
 java -jar target/${NAME}-${VERSION}.jar
