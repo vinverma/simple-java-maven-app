532b0bad958de454e6cc1a18b075f6f3eec23f03
vinverma <47500906+vinverma@users.noreply.github.com>
2024-11-15 00:48:29 +0000

    Update deliver.sh
    testing

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,9 +4,9 @@ echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
-#set -x
+
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
-#set +x
+
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
